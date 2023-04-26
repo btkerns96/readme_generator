36655977f79aa68b3f40e75bc79540470f12ef92
BrandonKerns <btkerns96@gmail.com>
2023-04-26 12:49:30 -0700

    Updated code

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-// TODO: Include packages needed for this application
+// Included Packages
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+// Create an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is your project title?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please provide a description of your project:',
+  },
+  // Add more questions for installation, usage, contribution, tests, license, GitHub username, and email, etc..
+];
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,11 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function renderLicenseBadge(license) {
+  // Add code to render the license badge
 }
 
-module.exports = generateMarkdown;
+function renderLicenseLink(license) {
+  // Add code to render the license link
+}
+
+function renderLicenseSection(license) {
+  // Add code to render the license section
+}
